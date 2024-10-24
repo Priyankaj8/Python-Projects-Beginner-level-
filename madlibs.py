b1e0b005b6660a1246821e25640aaae221196ead
Priyankaj8 <821priyankaj@gmail.com>
2024-10-24 13:26:06 +0530

    Re-push python file

---
+++ madlibs.py
@@ -0,0 +1,22 @@
+adj1 = input("Adjective: ")
+color = input("Color: ")
+noun1 = input("Noun: ")
+plural_noun1 = input("Plural noun: ")
+persons_name = input("Person's Name: ")
+place = input("Place: ")
+adverb = input("Adverb: ")
+plural_noun2 = input("Plural noun: ")
+noun2 = input("Noun: ")
+verb = input("Verb: ")
+plural_noun3 = input("Plural noun: ")
+
+madlib = f"One {adj1} day, I was walking my {color} \
+ pet {noun1} when {plural_noun1} started flying\
+ from the sky! I called my friend {persons_name}\
+ and she said one just landed right on her {place}\
+ ! {adverb}, there was raining {plural_noun2} as\
+ well and they were just going everywhere! My\
+{noun2} started {verb} and catching {plural_noun3}\
+with his mouth to eat."
+
+print(madlib)
